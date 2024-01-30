53b5176097f76e8e6131dee0d445920fa8c590f9
deivid94 <md031194@gmail.com>
2024-01-30 01:38:01 -0300

    feat lacos de repeticao

---
+++ src/lacoReperticaoFor.java
@@ -0,0 +1,10 @@
+public class  lacoReperticaoFor {
+    public static void main(String[] args) {
+        int contator = 0;
+        for (int i = 1; i <= 10 ; i++){
+            contator++;
+        }
+        System.out.println(contator);
+    }
+    
+}
