bede78ab777e5b362de89bfedea382d0e2307c76
deivid94 <md031194@gmail.com>
2024-01-30 08:02:43 -0300

    feat criando escopo projeto final

---
+++ src/ProjetoFInal.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class ProjetoFInal { 
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Digite um numero interio: ");
+        int valorDigitado = sc.nextInt();
+        System.out.println("o numero digitado foi: " + valorDigitado);
+    }
+}
